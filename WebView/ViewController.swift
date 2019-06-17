1160a9e8b0e7f8edc8b282b36cf452365cf9c684
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-06-17 19:46:53 -0300

    getting request data

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        if let url = URL(string: "https://stackoverflow.com") {
+//            webview.loadRequest(URLRequest(url: url))
+//        }
+        
+        if let url = URL(string: "https://stackoverflow.com") {
+            let request = NSMutableURLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                if let error = error {
+                    print(error)
+                } else {
+                    if let unwrappedData = data {
+                        if let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) {
+                            print(dataString)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
     }
-
-
 }
 
